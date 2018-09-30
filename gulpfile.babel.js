aa585c335588c8e388304dfa8e0a8dfb3f6c5112
Malgorzata Dominikowska <m.dominikowska@wp.pl>
2018-09-30 22:00:45 +0200

    config modifications

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     return gulp.src('scss/main.scss')
@@ -11,6 +12,7 @@ gulp.task('sass', function() {
             errLogToConsole: true,
             outputStyle: 'expanded'
         }))
+        .pipe(autoprefixer())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'))
 });
