c2fff3aeba949ceba444597f63207e81a9742038
Malgorzata Dominikowska <m.dominikowska@wp.pl>
2018-10-04 14:10:24 +0200

    google map

--- js/main.js
+++ js/main.js
@@ -31,7 +31,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
          } else {
              this.innerText = "SHOW ME MORE"
          }
-
+          
       })
-
 });
+
+//map
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+} 
